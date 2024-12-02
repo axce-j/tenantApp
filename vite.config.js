a08fb681d32bc46438680919fc69aa4a626587c6
axce-j <johnobi699@gmail.com>
2024-12-02 13:01:08 +0000

    delete dist

--- vite.config.js
+++ vite.config.js
@@ -6,46 +6,46 @@ export default defineConfig({
   base: "/tenantapp", // Base URL for your app
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate', // Automatically update the service worker
-      // manifest: {
-      //   name: "TenantApp",
-      //   short_name: "TenantApp",
-      //   description: "A tenant management application.",
-      //   start_url: "/tenantapp/index.html",
-      //   scope: "/tenantapp/",
-      //   display: "standalone",
-      //   background_color: "#ffffff",
-      //   theme_color: "#000000",
-      //   icons: [
-      //     {
-      //       src: "/src/assets/home-page-brown.png",
-      //       sizes: "512x512",
-      //       type: "image/png"
-      //     },
-      //     {
-      //       src: "/src/assets/home-page-brown-2.png",
-      //       sizes: "512x512",
-      //       type: "image/png"
-      //     }
-      //   ]
-      // },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /.*\.(js|css|html|png|jpg|svg)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'tenantapp-assets',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
-              }
-            }
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate', // Automatically update the service worker
+    //   // manifest: {
+    //   //   name: "TenantApp",
+    //   //   short_name: "TenantApp",
+    //   //   description: "A tenant management application.",
+    //   //   start_url: "/tenantapp/index.html",
+    //   //   scope: "/tenantapp/",
+    //   //   display: "standalone",
+    //   //   background_color: "#ffffff",
+    //   //   theme_color: "#000000",
+    //   //   icons: [
+    //   //     {
+    //   //       src: "/src/assets/home-page-brown.png",
+    //   //       sizes: "512x512",
+    //   //       type: "image/png"
+    //   //     },
+    //   //     {
+    //   //       src: "/src/assets/home-page-brown-2.png",
+    //   //       sizes: "512x512",
+    //   //       type: "image/png"
+    //   //     }
+    //   //   ]
+    //   // },
+    //   workbox: {
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /.*\.(js|css|html|png|jpg|svg)$/,
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'tenantapp-assets',
+    //           expiration: {
+    //             maxEntries: 50,
+    //             maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // })
   ]
 });
 // ff
